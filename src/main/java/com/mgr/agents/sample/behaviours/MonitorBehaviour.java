9e79c7fe85fa716ceaa33c7ad4ab4f416938d825
Krzysiek <kkamins3@gmail.com>
2016-04-26 18:34:56 +0200

    Basic impelemntation of MonitorBehavioru

--- src/main/java/com/mgr/agents/sample/behaviours/MonitorBehaviour.java
+++ src/main/java/com/mgr/agents/sample/behaviours/MonitorBehaviour.java
@@ -1,5 +1,6 @@
 package com.mgr.agents.sample.behaviours;
 
+import com.mgr.agents.sample.utility.Status;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 
@@ -7,12 +8,14 @@ import jade.core.behaviours.TickerBehaviour;
  * Created by a on 19.04.16.
  */
 public class MonitorBehaviour extends TickerBehaviour {
-    public MonitorBehaviour(Agent a, long period) {
+    Status status;
+    public MonitorBehaviour(Agent a, Status s, long period) {
         super(a, period);
+        this.status = s;
     }
 
     @Override
     protected void onTick() {
-
+        status.update();
     }
 }
