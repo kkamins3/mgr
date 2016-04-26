644b8949cae2d2447577c2012046d7870612cab9
Krzysiek <kkamins3@gmail.com>
2016-04-26 18:37:56 +0200

    Basic impelemntation of Task waiting beaviour

--- src/main/java/com/mgr/agents/sample/behaviours/TaskWaitBehaviour.java
+++ src/main/java/com/mgr/agents/sample/behaviours/TaskWaitBehaviour.java
@@ -1,4 +1,5 @@
 package com.mgr.agents.sample.behaviours;
+import com.mgr.agents.sample.utility.*;
 import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.lang.acl.ACLMessage;
@@ -8,9 +9,10 @@ import jade.lang.acl.MessageTemplate;
  * Created by a on 19.04.16.
  */
 public class TaskWaitBehaviour extends CyclicBehaviour {
-    private final Agent agent;
-    public TaskWaitBehaviour(Agent agent) {
-        this.agent = agent;
+    private final Status status;
+    public TaskWaitBehaviour(Agent a, Status s) {
+        super(a);
+        this.status = s;
     }
 
     public void action() {
@@ -18,20 +20,20 @@ public class TaskWaitBehaviour extends CyclicBehaviour {
         ACLMessage msg = myAgent.receive(mt);
         if (msg != null) {
             // CFP Message received. Process it
-            String title = msg.getContent();
+            String task = msg.getContent();
             ACLMessage reply = msg.createReply();
 
-//            Integer price = (Integer) catalogue.get(title);
-//            if (price != null) {
+            Integer estimate = status.estimate(task);
+            if (estimate != null) {
                 // The requested book is available for sale. Reply with the price
                 reply.setPerformative(ACLMessage.PROPOSE);
-//                reply.setContent(String.valueOf(price.intValue()));
-//            }
-//            else {
+                reply.setContent(String.valueOf(estimate.intValue()));
+            }
+            else {
                 // The requested book is NOT available for sale.
-//                reply.setPerformative(ACLMessage.REFUSE);
-//                reply.setContent("not-available");
-//            }
+                reply.setPerformative(ACLMessage.REFUSE);
+                reply.setContent("not-interested");
+            }
 //            myAgent.send(reply);
         }
         else {
