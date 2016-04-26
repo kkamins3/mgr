50b212a328e123fc0240691e1550fa47c3ff0cd0
Krzysiek <kkamins3@gmail.com>
2016-04-26 18:32:01 +0200

    Add utility classes

--- src/main/java/com/mgr/agents/sample/utility/Status.java
+++ src/main/java/com/mgr/agents/sample/utility/Status.java
@@ -1,7 +1,25 @@
 package com.mgr.agents.sample.utility;
 
+import java.util.LinkedList;
+
 /**
  * Created by a on 19.04.16.
  */
 public class Status {
+    LinkedList<Task> taskList = new LinkedList<>();
+    public Integer estimate(String task) {
+        return null;
+    }
+
+    public Boolean addTask(String task) {
+        return true;
+    }
+
+    public boolean update() {
+        return true;
+    }
+
+    public boolean isAvailable() {
+        return true;
+    }
 }
--- src/main/java/com/mgr/agents/sample/utility/Task.java
+++ src/main/java/com/mgr/agents/sample/utility/Task.java
@@ -4,4 +4,7 @@ package com.mgr.agents.sample.utility;
  * Created by a on 19.04.16.
  */
 public class Task {
+    public Task(String task) {
+
+    }
 }
