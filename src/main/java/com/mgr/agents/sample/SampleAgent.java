91c94ecfe5e3e2e6d28c6cead5d1e6cde94a0a29
Krzysiek <kkamins3@gmail.com>
2016-04-29 23:26:27 +0200

    Create startup behaviours in Agent

--- src/main/java/com/mgr/agents/sample/SampleAgent.java
+++ src/main/java/com/mgr/agents/sample/SampleAgent.java
@@ -1,6 +1,10 @@
 package com.mgr.agents.sample;
 
 import com.mgr.agents.sample.behaviours.IncrementBaseNumber;
+import com.mgr.agents.sample.behaviours.MonitorBehaviour;
+import com.mgr.agents.sample.behaviours.ReceiveTaskBehaviour;
+import com.mgr.agents.sample.behaviours.TaskWaitBehaviour;
+import com.mgr.agents.sample.utility.Position;
 import com.mgr.agents.sample.utility.Status;
 import com.mgr.agents.sample.utility.Task;
 import jade.core.Agent;
@@ -8,14 +12,18 @@ import jade.util.leap.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.awt.*;
+
 public class SampleAgent extends Agent {
-    private final Status status = new Status();
+    private final Status status = new Status(new Position(new Point(0, 0), 0));
     private static final Logger logger = LoggerFactory.getLogger(SampleAgent.class);
 
     @Override
     public void setup() {
         final String otherAgentName = (String) this.getArguments()[0];
-        addBehaviour(new IncrementBaseNumber(this, otherAgentName));
+        addBehaviour(new TaskWaitBehaviour(this, status));
+        addBehaviour(new ReceiveTaskBehaviour(this, status));
+        addBehaviour(new MonitorBehaviour(this, status, 20));
     }
 
     @Override
