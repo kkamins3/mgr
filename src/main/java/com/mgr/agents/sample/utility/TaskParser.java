7b8a71ef8bb0f5334ebd25007f7ce21d673f7925
Krzysiek <kkamins3@gmail.com>
2016-04-29 23:18:03 +0200

    Create TaskParser skeleton

---
+++ src/main/java/com/mgr/agents/sample/utility/TaskParser.java
@@ -0,0 +1,14 @@
+package com.mgr.agents.sample.utility;
+
+import jdk.internal.util.xml.impl.Pair;
+
+import java.awt.*;
+
+/**
+ * Created by a on 29.04.16.
+ */
+public class TaskParser {
+    static public Position parseTask(String task) {
+        return new Position(new Point(10,10), 10);
+    }
+}
