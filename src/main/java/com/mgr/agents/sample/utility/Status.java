28fcedc075768c6896dd4c8eb463345a7ab5c5b0
Krzysiek <kkamins3@gmail.com>
2016-04-29 23:25:20 +0200

    Update Test and Status utilities

--- src/main/java/com/mgr/agents/sample/utility/Status.java
+++ src/main/java/com/mgr/agents/sample/utility/Status.java
@@ -1,18 +1,50 @@
 package com.mgr.agents.sample.utility;
 
+import java.awt.*;
 import java.util.LinkedList;
 
 /**
  * Created by a on 19.04.16.
  */
 public class Status {
-    LinkedList<Task> taskList = new LinkedList<>();
+    private Position currentPosition;
+    private LinkedList<Task> taskList = new LinkedList<>();
+
+    public Status(Position currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+
     public Integer estimate(String task) {
-        return null;
+        Task t = new Task(task);
+        if (t == null) {
+            return -1;
+        }
+        Integer estimation = new Integer(0);
+        Position previousPosition = new Position(currentPosition);
+        for (Task element: taskList) {
+            estimation += element.estimate(previousPosition);
+            previousPosition = element.getDestination();
+        }
+        estimation += t.estimate(previousPosition);
+        return estimation;
     }
 
     public Boolean addTask(String task) {
+        Task t = new Task(task);
+        if (t == null) {
+            return false;
+        }
+        taskList.push(t);
         return true;
+
+    }
+
+    public Task getTask() {
+        return taskList.pop();
+    }
+
+    public Position getCurrentPosition() {
+        return currentPosition;
     }
 
     public boolean update() {
--- src/main/java/com/mgr/agents/sample/utility/Task.java
+++ src/main/java/com/mgr/agents/sample/utility/Task.java
@@ -4,7 +4,37 @@ package com.mgr.agents.sample.utility;
  * Created by a on 19.04.16.
  */
 public class Task {
-    public Task(String task) {
+    private final Integer riseSpeed = new Integer(2);
+    private final Integer fallSpeed = new Integer(1);
+    private final Integer horizontalSpeed = new Integer(4);
+    private final Integer recordingConstant = new Integer(120);
+    private Integer estimation = null;
+    private final String task;
 
+    public Integer getEstimation() {
+        return estimation;
+    }
+
+    private Position destination;
+    public Task(String t) {
+        task = t;
+    }
+
+    public int estimate(Position position) {
+        if (estimation == null) {
+            estimation = new Integer(0);
+            estimation += Math.abs(position.getPosition().x - destination.getPosition().x) * horizontalSpeed;
+            estimation += Math.abs(position.getPosition().y - destination.getPosition().y) * horizontalSpeed;
+            estimation += Math.abs(position.getHeight() - destination.getHeight()) * riseSpeed;
+            estimation += recordingConstant;
+        }
+        return estimation;
+    }
+
+    public Position getDestination() {
+        if (destination == null) {
+            destination = TaskParser.parseTask(task);
+        }
+        return destination;
     }
 }
